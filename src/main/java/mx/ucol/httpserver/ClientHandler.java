8b4d2ac024cae8dcede8c66e2c290ad668c1efc3
RDIPIPOM <ezamora2@ucol.mx>
2021-05-30 20:29:42 -0500

    Third update. Everything's working

--- src/main/java/mx/ucol/httpserver/ClientHandler.java
+++ src/main/java/mx/ucol/httpserver/ClientHandler.java
@@ -2,7 +2,10 @@ package mx.ucol.httpserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ClientHandler implements Runnable {
   final Socket socket;
@@ -12,13 +15,11 @@ public class ClientHandler implements Runnable {
   }
 
   public void run() {
-    PrintWriter output = null;
+    DataOutputStream output = null;
     BufferedReader input = null;
-    File HTMLFile = null;
-    String htmlResponse = "";
 
     try {
-      output = new PrintWriter(socket.getOutputStream(), true);
+      output = new DataOutputStream(socket.getOutputStream());
       input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
       String received;
@@ -26,26 +27,31 @@ public class ClientHandler implements Runnable {
         String requestArray[] = received.split(" ");
 
         if (requestArray[0].equals("GET")) {
-          System.out.println("Resource: " + requestArray[1]);
-          switch (requestArray[1]){
-            case "/":
-            case "/index.html":
-              HTMLFile = new File("./www/index.html");
-              break;
-            case "/about":
-            case "/about.html":
-              HTMLFile = new File("./www/about.html");
-              break;
-          }
-          Scanner scanner = new Scanner(HTMLFile);
-          while (scanner.hasNextLine())
-            htmlResponse = htmlResponse + scanner.nextLine();
-          scanner.close();
-          int contentLength = htmlResponse.length();
+          String resourceName = requestArray[1].equals("/") ? "/index.html" : requestArray[1];
+          String resourcePath = "./www" + resourceName;
+          System.out.println("Resource: " + resourcePath);
+          Path filePath =  Paths.get(resourcePath);
 
-          // This line should not be modified just yet
-          output.write("HTTP/1.1 200 OK\r\nContent-Length: " +
-            String.valueOf(contentLength) + "\r\n\r\n" + htmlResponse);
+          boolean fileExists = Files.exists(filePath, LinkOption.NOFOLLOW_LINKS);
+
+          if(!fileExists) filePath = Paths.get("./www/not-found.html");
+
+          String response = null;
+          byte[] fileContent = null;
+          int contentLength = 0;
+
+          response = fileExists ? "HTTP/1.1 200 OK\r\n" : "HTTP/1.1 404\r\n";
+
+          fileContent = Files.readAllBytes(filePath);
+          contentLength = fileContent.length;
+          String mimeType = Files.probeContentType(filePath);
+          System.out.println("Content-Type: " + mimeType);
+
+          response += "Content-Type: " + mimeType + "\r\n";
+          response += "Content-Length: " + String.valueOf(contentLength) + "\r\n\r\n";
+
+          output.writeBytes(response);
+          output.write(fileContent, 0, contentLength);
 
           // We already sent the response, break the loop
           break;
